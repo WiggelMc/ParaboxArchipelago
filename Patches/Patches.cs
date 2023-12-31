833452f2d58c654a7035c1ea35f8fce214fc0cd4
WiggelMc <78086343+WiggelMc@users.noreply.github.com>
2023-12-31 18:55:44 +0100

    Add patches for Resource Patching

---
+++ Patches/Patches.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HarmonyLib;
+using Mono.Cecil;
+
+namespace ParaboxArchipelago.Patches
+{
+    public class Patches
+    {
+        // List of assemblies to patch
+        public static IEnumerable<string> TargetDLLs { get; } = new[] {"UnityEngine.CoreModule.dll"};
+
+        // Patches the assemblies
+        public static void Patch(AssemblyDefinition assembly)
+        {
+            // Patcher code here
+            assembly.Modules
+                .SelectMany(m => m.Types)
+                .Where(t => t.Namespace == "UnityEngine" && t.Name == "Resources")
+                .SelectMany(t => t.Methods)
+                .Where(m => m.Name == "Load" && !m.HasGenericParameters)
+                .Do(m =>
+                {
+                    
+                });
+        }
+    }
+}
---
+++ Patches/Resources.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using HarmonyLib;
+using Mono.Cecil;
+using MonoMod.Utils;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace ParaboxArchipelago.Patches
+{
+    public static class Resources
+    {
+        public class Prefs_GetPrefsFilePath
+        {
+            private static MethodBase TargetMethod() => AccessTools.Method(typeof(UnityEngine.Resources),
+                nameof(UnityEngine.Resources.Load), new[] { typeof(string), typeof(Type) });
+            public static bool Prefix(ref Object __result, string path, Type systemTypeInstance)
+            {
+                if (path == "hub")
+                {
+                    __result = new TextAsset("version 4\n#\n");
+                    return false;
+                }
+
+                return true;
+            }
+            
+            public static void Postfix(ref Object __result, string path, Type systemTypeInstance)
+            {
+                
+            }
+        }
+    }
+}
