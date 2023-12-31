0aaa68cda655bc2d3b6cccb5c991d90cf0bd8c19
WiggelMc <78086343+WiggelMc@users.noreply.github.com>
2023-12-31 16:54:43 +0100

    Fix Versioning

---
+++ ParaboxArchipelago.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+
+namespace ParaboxArchipelago
+{
+    [BepInPlugin(ParaboxPluginInfo.PluginGuid, ParaboxPluginInfo.PluginName, ParaboxPluginInfo.PluginVersion)]
+    [BepInProcess("Patrick's Parabox.exe")]
+    public class ParaboxArchipelago : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {ParaboxPluginInfo.PluginGuid} is loaded!");
+        }
+    }
+}
---
+++ ParaboxPluginInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace ParaboxArchipelago
+{
+    public static class ParaboxPluginInfo
+    {
+        public const string PluginGuid = "wiggelmc.archipelago.parabox";
+        public const string PluginName = "Archipelago Randomizer";
+        public const string PluginVersion = PluginInfo.PLUGIN_VERSION;
+    }
+}
