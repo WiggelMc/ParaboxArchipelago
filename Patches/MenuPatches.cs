1e116630bf921bfbfca8923cb1f86944078f0df8
WiggelMc <78086343+WiggelMc@users.noreply.github.com>
2024-01-01 21:27:34 +0100

    make Window Relative

--- Patches/MenuPatches.cs
+++ Patches/MenuPatches.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using HarmonyLib;
 using ParaboxArchipelago.State;
@@ -52,48 +53,12 @@ namespace ParaboxArchipelago.Patches
                         GUI.Button(new Rect(0, i*30+10, 260, 20), "Game " + i);
                     }
                     GUI.EndScrollView();
-
-                    var windowTexture = new Texture2D(1, 1);
-                    windowTexture.SetPixel(1,1,new Color(0,0,0,0.1f));
-                    menuState.TestWindowRect = GUI.Window(0, menuState.TestWindowRect, (id) =>
+                    
+                    
+                    menuState.ConnectionWindowRect.RelativeRect = DrawElement(menuState.ConnectionWindowRect.RelativeRect, bounds =>
                     {
-                        var windowRect = menuState.TestWindowRect;
-                        var dragTexture = new Texture2D(1, 1);
-                        dragTexture.SetPixel(1,1,new Color(1,1,1,0.2f));
-                        var dragMargin = 20;
-                        var dragRects = new[]
-                        {
-                            new Rect(0, 0, windowRect.width, dragMargin),
-                            new Rect(0, windowRect.height - dragMargin, windowRect.width, dragMargin),
-                            new Rect(0, dragMargin, dragMargin, windowRect.height - 2*dragMargin),
-                            new Rect(windowRect.width - dragMargin, dragMargin, dragMargin, windowRect.height - 2*dragMargin)
-                        };
-                        foreach (var dragRect in dragRects)
-                        {
-                            GUI.Box(dragRect, dragTexture, new GUIStyle()
-                            {
-                                border = new RectOffset(),
-                                normal = new GUIStyleState()
-                                {
-                                    background = dragTexture
-                                }
-                            });
-                            GUI.DragWindow(dragRect);
-                        }
-                    }, windowTexture, new GUIStyle()
-                    {
-                        normal = new GUIStyleState()
-                        {
-                            background = windowTexture
-                        }
-                    });
-
-                    menuState.TestWindowRect = new Rect(
-                        Mathf.Clamp(menuState.TestWindowRect.x, 0, Draw.screenWidth - menuState.TestWindowRect.width),
-                        Mathf.Clamp(menuState.TestWindowRect.y, 0, Draw.screenHeight - menuState.TestWindowRect.height),
-                        menuState.TestWindowRect.width,
-                        menuState.TestWindowRect.height
-                    );
+                        
+                    }, true, 0);
                     
                     
                     var focusedControlName = GUI.GetNameOfFocusedControl();
@@ -140,6 +105,118 @@ namespace ParaboxArchipelago.Patches
         {
             GUI.SetNextControlName(name);
             return GUI.Button(position, text);
+        }
+
+        
+        private static readonly Texture2D InvisibleTexture;
+        
+        private static readonly Texture2D DragRectTexture;
+        private static readonly GUIStyle DragRectStyle;
+        
+        private static readonly Texture2D DragWindowTexture;
+        private static readonly GUIStyle DragWindowStyle;
+        static MenuPatches()
+        {
+            var invisibleTexture = new Texture2D(1,1);
+            invisibleTexture.SetPixel(1,1,new Color(0,0,0,0));
+            InvisibleTexture = invisibleTexture;
+            
+            var dragRectTexture = new Texture2D(1,1);
+            dragRectTexture.SetPixel(1,1,new Color(1,1,1,0.2f));
+            DragRectTexture = dragRectTexture;
+            DragRectStyle = new GUIStyle
+            {
+                border = new RectOffset(),
+                normal = new GUIStyleState
+                {
+                    background = dragRectTexture
+                }
+            };
+            
+            var dragWindowTexture = new Texture2D(1,1);
+            dragWindowTexture.SetPixel(1,1,new Color(0,0,0,0.2f));
+            DragWindowTexture = dragWindowTexture;
+            DragWindowStyle = new GUIStyle()
+            {
+                normal = new GUIStyleState()
+                {
+                    background = dragWindowTexture
+                }
+            };
+        }
+        
+        
+        
+        private static Rect DrawElement(Rect relativeBounds, Action<Rect> drawContent, bool drawWindow, int windowID)
+        {
+            var screenWidth = Draw.screenWidth;
+            var screenHeight = Draw.screenHeight;
+            var absoluteBounds = ToAbsolute(relativeBounds, screenWidth, screenHeight);
+
+            Rect newRelativeBounds;
+            if (drawWindow)
+            {
+                var newAbsoluteBounds = GUI.Window(windowID, absoluteBounds, _ =>
+                {
+                    DrawWindowControls(absoluteBounds);
+                    drawContent.Invoke(absoluteBounds);
+                }, DragWindowTexture, DragWindowStyle);
+                newRelativeBounds = ToRelative(newAbsoluteBounds, screenWidth, screenHeight);
+            }
+            else
+            {
+                drawContent.Invoke(absoluteBounds);
+                newRelativeBounds = relativeBounds;
+            }
+
+            return ClampRelative(newRelativeBounds);
+        }
+
+        private static Rect ToRelative(Rect absolute, float referenceWidth, float referenceHeight)
+        {
+            return new Rect(
+                absolute.x / referenceWidth,
+                absolute.y / referenceHeight,
+                absolute.width / referenceWidth,
+                absolute.height / referenceHeight
+            );
+        }
+        
+        private static Rect ToAbsolute(Rect relative, float referenceWidth, float referenceHeight)
+        {
+            return new Rect(
+                relative.x * referenceWidth,
+                relative.y * referenceHeight,
+                relative.width * referenceWidth,
+                relative.height * referenceHeight
+            );
+        }
+        
+        private static Rect ClampRelative(Rect relative)
+        {
+            return new Rect(
+                Mathf.Clamp(relative.x, 0, 1 - relative.width),
+                Mathf.Clamp(relative.y, 0, 1 - relative.height),
+                Mathf.Clamp01(relative.width),
+                Mathf.Clamp01(relative.height)
+            );
+        }
+
+        private static void DrawWindowControls(Rect absoluteBounds)
+        {
+            const int dragMargin = 20;
+            var dragRects = new[]
+            {
+                new Rect(0, 0, absoluteBounds.width, dragMargin),
+                new Rect(0, absoluteBounds.height - dragMargin, absoluteBounds.width, dragMargin),
+                new Rect(0, dragMargin, dragMargin, absoluteBounds.height - 2*dragMargin),
+                new Rect(absoluteBounds.width - dragMargin, dragMargin, dragMargin, absoluteBounds.height - 2*dragMargin)
+            };
+            foreach (var dragRect in dragRects)
+            {
+                GUI.Box(dragRect, DragRectTexture, DragRectStyle);
+                GUI.DragWindow(dragRect);
+            }
         }
 
         private static GUIStyle InitGUIStyle()
--- State/MenuState.cs
+++ State/MenuState.cs
@@ -10,7 +10,32 @@ namespace ParaboxArchipelago.State
         public bool IsInTextField { get; set; } = false;
         public float GuiKeyLastPressTime { get; set; } = 0f;
         public Vector2 RecentGamesScrollPosition { get; set; } = Vector2.zero;
-        public Rect TestWindowRect { get; set; } = new(300, 10, 200, 200);
         public bool EnableWindowMove { get; set; } = false;
+
+        public WindowState PersonalItemFeedWindowRect { get; set; } = new(0.2f, 0, 0.2f, 0.2f);
+        public WindowState OtherItemFeedWindowRect { get; set; } = new(0.4f, 0, 0.2f, 0.2f);
+        public WindowState ChatWindowRect { get; set; } = new(0.6f, 0, 0.2f, 0.2f);
+        public WindowState ItemTrackerWindowRect { get; set; } = new(0.8f, 0, 0.2f, 0.2f);
+        public WindowState LocationTrackerWindowRect { get; set; } = new(0.8f, 0.2f, 0.2f, 0.2f);
+        public WindowState ConnectionWindowRect { get; set; } = new(0, 0.2f, 0.2f, 0.2f);
+        
+        public class WindowState
+        {
+            public Rect RelativeRect { get; set; }
+            public WindowInteractionState MenuState { get; set; }
+            public WindowInteractionState OverlayState { get; set; }
+
+            public WindowState(float x, float y, float width, float height)
+            {
+                RelativeRect = new Rect(x, y, width, height);
+                MenuState = WindowInteractionState.Hidden;
+                OverlayState = WindowInteractionState.Hidden;
+            }
+        }
+        
+        public enum WindowInteractionState
+        {
+            Hidden, View, Interact
+        }
     }
 }
